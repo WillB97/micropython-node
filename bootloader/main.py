841fc9a724e469552d98f43b01b37017236b0971
WillB97 <will@rudi.org.uk>
2026-06-28 10:32:08 +0100

    Fix resetting on code failure

--- bootloader/main.py
+++ bootloader/main.py
@@ -5,14 +5,15 @@ sys.path.insert(1, '/active')
 
 try:
     import active.main
-except Exception as e:
-    print("Fatal error in main:")
-    sys.print_exception(e)
 except KeyboardInterrupt:
     pass
-else:
+except Exception as e:
     # Following a normal Exception or main() exiting, reset the board.
     # Following a non-Exception error such as KeyboardInterrupt (Ctrl-C),
     # this code will drop to a REPL. Place machine.reset() in a finally
     # block to always reset, instead.
+    machine.reset()
+    print("Fatal error in main:")
+    sys.print_exception(e)
+else:
     machine.reset()
